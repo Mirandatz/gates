d77eefb165fda4b6ada9911c780b0a4fe3f9eefc
MirandaTZ <thiago.zafalon.miranda@gmail.com>
2023-04-09 13:45:33 -0300

    Improve type hints in fitness.py

--- gates/fitnesses.py
+++ gates/fitnesses.py
@@ -6,26 +6,29 @@ from sklearn.metrics import accuracy_score
 
 import gates.nor_gates as ng
 
-T = typing.TypeVar("T")
-
-EvalutedIndividual = tuple[T, float]
+EvalutedIndividual = tuple[ng.NorClassifier, float]
+Fitness = float
 
 
 def evaluate_fitness(
     individual: ng.NorClassifier,
-    features: npt.NDArray[np.bool8],
-    targets: npt.NDArray[np.bool8],
-) -> float:
+    features: npt.NDArray[np.bool_],
+    targets: npt.NDArray[np.bool_],
+) -> Fitness:
     predicted = ng.parallel_predict_dataset(individual, features)
     accuracy = accuracy_score(targets, predicted)
     assert isinstance(accuracy, float)
     return accuracy
 
 
+T = typing.TypeVar("T")
+
+
 def select_fittest(
-    individuals: list[EvalutedIndividual[T]],
+    individuals: list[tuple[T, Fitness]],
     fittest_count: int,
-) -> list[EvalutedIndividual[T]]:
+) -> list[tuple[T, Fitness]]:
+    assert fittest_count >= 1
     assert fittest_count <= len(individuals)
 
     from_best_to_worst = sorted(
